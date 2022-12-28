acbb249cdc7a8e9c8cd0a6830562786624b2e101
vinnywalker96 <vinnywalker96@icloud.com>
2022-12-28 15:38:45 +0000

    8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-   if key == a_dictionary.keys():
-       a_dictionary.pop(key)
-   return a_dictionary
+    if key == a_dictionary.keys():
+        a_dictionary.pop(key)
+    return a_dictionary
