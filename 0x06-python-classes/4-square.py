331742b6e7eb5017eafbc1b3fdc2ad1b3b9fb0e5
vinnywalker96 <vinnywalker96@icloud.com>
2023-01-02 15:17:29 +0000

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,7 +11,7 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
