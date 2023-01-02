44f5345e9f2e3f3bd4831e11be2f80d24f151e9d
vinnywalker96 <vinnywalker96@icloud.com>
2023-01-02 11:34:32 +0000

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Define class of Square"""
+
+
+class Square:
+    """Representation of a Square"""
+
+    def __init__(self, size):
+        """Initilizing a new Square Object
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
