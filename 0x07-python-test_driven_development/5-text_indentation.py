96a895f1cc96cafa06bf5ea82a73e79b1ed57681
vinnywalker96 <vinnywalker96@icloud.com>
2023-01-08 15:20:25 +0000

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,7 +4,8 @@
 
 
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters:
+    """Prints a text with 2 new lines 
+       after each of these characters:
 
     Args:
         text (str): argument as form of a string
@@ -13,12 +14,15 @@ def text_indentation(text):
         TypeError: if text is not a string
 
     """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
     string_len = 0
 
     while string_len < len(text) and text[string_len] == " ":
         string_len += 1
     while string_len < len(text):
-        print(text[string_len] , end="")
+        print(text[string_len], end="")
         if text[string_len] in ".?:":
             print("\n")
         string_len += 1
