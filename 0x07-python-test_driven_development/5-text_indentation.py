6104d22d4ff7f199d87170d55434180c047f43ea
vinnywalker96 <vinnywalker96@icloud.com>
2023-01-08 14:57:08 +0000

    5-text_indentation.py

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""Defines function at that indent text"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters:
+
+    Args:
+        text (str): argument as form of a string
+
+    Raises:
+        TypeError: if text is not a string
+
+    """
+    string_len = 0
+
+    while string_len < len(text) and text[string_len] == " ":
+        string_len += 1
+    while string_len < len(text):
+        print(text[string_len] , end="")
+        if text[string_len] in ".?:":
+            print("\n")
+        string_len += 1
+        while string_len < len(text) and text[string_len] == " ":
+            string_len += 1
+        continue
+    string_len += 1
