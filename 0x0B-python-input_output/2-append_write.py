ee3117e61f7183e190106b4289faa94999537a97
vinnywalker96 <vinnywalker96@icloud.com>
2023-01-12 22:38:38 +0000

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,12 +3,12 @@
 
 
 def append_write(filename="", text=""):
-    """Write a string to a UTF8 text file.
+    """Append a string to a UTF8 text file.
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
         The number of characters written.
     """
-    with open(filename, encoding="utf-8", mode="a") as f:
+    with open(filename, encoding="utf-8", mode="a+") as f:
         return f.write()
