a14c40ce532b37adc3b5a4465bb188b9e9a3298b
vinnywalker96 <vinnywalker96@icloud.com>
2023-01-18 12:53:12 +0000

    adding the update public method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,6 +28,60 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
+    def update(self, *args, **kwargs):
+        """Update the Rectangle.
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.size,
+                                      self.x, self.y)
+                    else:
+                        self.id = arg
+
+                elif a == 1:
+                    self.size = arg
+
+                elif a == 2:
+                    self.size = arg
+
+                elif a == 3:
+                    self.x = arg
+
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        if kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    if value is None:
+                        self.__init__(self.size, self.size,
+                                      self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == 'size':
+                    self.size = value
+
+                elif key == 'size':
+                    self.size = value
+
+                elif key == 'x':
+                    self.x = value
+
+                elif key == 'y':
+                    self.y = value
+
     def __str__(self):
         """Return the print() and str() representation of a Square."""
         return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
