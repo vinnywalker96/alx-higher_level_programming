91df245d8539816e5bc91f7ea61bae860e5491c7
vinnywalker96 <vinnywalker96@icloud.com>
2023-01-24 13:57:08 +0000

    test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -14,12 +14,19 @@ class TestRectangle_initialization(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle()
 
+    def test_two_args(self):
+        r1 = Rectangle(1, 2)
+        r2 = Rectangle(2, 1)
+        self.assertEqual(r1.id, r2.id - 1)
+
+
+
 class TestRectangle_area(unittest.TestCase):
     """Test Area"""
 
     def test_area_args(self):
         r1 = Rectangle(1, 2)
         self.assertEqual(2, r1.area())
-        
+
 if __name__ == "__main__":
     unittest.main()
