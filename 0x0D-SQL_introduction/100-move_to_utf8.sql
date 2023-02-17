386c07694c6fa484e0a314e1458ad5cd9d9e5c85
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-02-17 09:09:07 +0000

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- This script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
