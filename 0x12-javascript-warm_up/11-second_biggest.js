a3a580f4bbed1f446afd3bf5cea1af4716b6eb8d
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-03-14 12:30:13 +0000

    13-add.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
+let arr = []; 
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-    
+    for (let i = 0; i < process.argv; i++) {
+	  console.log(process.argv[i]);
 }
+
+console.log(arr);
