476764cfae2a3d8d893fa8b858701c234ccefe78
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-03-17 23:13:47 +0000

    This script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script that lists all states with a name starting with N
+from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3],
+                         port=3306)
+    obj = db.cursor()
+    obj.execute("SELECT * FROM states WHERE\
+                states.name LIKE 'N%' ORDER BY states.id ASC")
+    rows = obj.fetchall()
+    for row in rows:
+        print(row)
+    obj.close()
+    obj.close()
