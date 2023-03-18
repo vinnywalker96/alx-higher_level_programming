36474841064da7a28fb4118d65bb74ffa2c51623
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-03-18 14:21:22 +0000

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that takes in arguments and
+displays all values in the states table """
+
+import sys
+import MySQLdb as db
+
+if __name__ == "__main__":
+    conn = db.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+    obj = conn.cursor()
+    obj.execute("SELECT * FROM states WHERE\
+                 name = '{}' ORDER BY id".format(sys.argv[4]))
+    rows = obj.fetchall()
+    for row in rows:
+        print(row)
+    obj.close()
+    conn.close()
