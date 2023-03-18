f45a055f6de8eae375b8110340dd6b1914f2b45b
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-03-18 16:38:35 +0000

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
             db=sys.argv[3],
             port=3307)
     obj = conn.cursor()
-    obj.execute("""SELECT cities.id, cities.name FROM
-                 cities INNER JOIN states ON states.id=cities.state_id""")
+    obj.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     result = obj.fetchall()
     for res in result:
         print(res)
