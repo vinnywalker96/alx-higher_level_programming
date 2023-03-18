6938294dcae32dd38e60f25646d27583d6c1dabd
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-03-18 15:15:43 +0000

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script that lists all cities from the database hbtn_0e_4_usa"""
+import sys
+import MySQLdb as db
+
+if __name__ == "__main__":
+    conn = db.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3307)
+    obj = conn.cursor()
+    obj.execute("""SELECT cities.id, cities.name FROM
+                 cities INNER JOIN states ON states.id=cities.state_id""")
+    result = obj.fetchall()
+    for res in result:
+        print(res)
+    obj.close()
+    conn.close()
