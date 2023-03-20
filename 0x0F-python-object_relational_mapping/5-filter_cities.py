f087a482139be0fef6b0fa72040686f4693c6a97
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-03-20 09:56:13 +0000

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
-"""This script that takes in the name 
+"""This script that takes in the name
 of a state as an argument and lists all cities
 """
 import sys
 import MySQLdb as db
 
 [USERNAME, PASSWORD, DB_NAME, STATE_NAME] = sys.argv[1:5]
-def filter_cities() -> None:
-    conn = db.connect()
-    
+
+
+def filter_cities():
+    conn = db.connect(
+           host='localhost',
+           user=USERNAME,
+           passwd=PASSWORD,
+           db=DB_NAME,
+           port=3306)
+    obj = conn.cursor()
+    obj.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON
+                states.id=cities.state_id
+                WHERE states.name=%s""", (STATE_NAME,))
+    rows = obj.fetchall()
+    res = list(row[0] for row in rows)
+    print(*res, sep=", ")
+    obj.close()
+    conn.close()
+
 
 if __name__ == "__main__":
     filter_cities()
