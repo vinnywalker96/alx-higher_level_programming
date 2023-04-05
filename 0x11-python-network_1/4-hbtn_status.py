4923a6f70ce1526f979db4454adacf90907d4a05
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-05 07:15:41 -0700

    Status coe

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches content from url
+display content
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:", type(req))
+    print("\t- content:", req.reason)
