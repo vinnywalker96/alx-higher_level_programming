9677e5c9274954bb58638d0527971db7116c08b9
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-05 07:18:05 -0700

    Status code

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,5 +10,5 @@ import requests
 if __name__ == "__main__":
     req = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type:", type(req))
+    print("\t- type:", type(req.text))
     print("\t- content:", req.reason)
