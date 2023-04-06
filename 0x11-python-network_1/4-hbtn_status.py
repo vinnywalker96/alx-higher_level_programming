f3c7efd332a7cefea46c9f12c9cc91c3dc42448d
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-05 23:48:35 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     res = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print('\t- type:', type(res.text))
-    print('\t- content:', res.reason)
+    print('\t- content:', res.text)
