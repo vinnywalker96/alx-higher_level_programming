065e01059112fe25e956f20e59222ebb6af21cd7
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-06 04:10:15 -0700

    Authenticate Github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Connect to Github using Github Api
+   Args:
+       username: str
+       password: PAT
+   Return: user id
+"""
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    basic = HTTPBasicAuth(username, password)
+    req = requests.get(f'https://api.github.com/users/{username}', auth=basic)
+    data = req.json()
+    print(data['id'])
