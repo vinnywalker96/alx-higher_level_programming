4960b2928dc66320c9dc79781051bd0aa2e6fa73
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-04-10 08:06:43 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 
 const list = require('./100-data').list; // Import array from file
 
-let newList = list.map((value, index) => value * index); // Map to compute new array
+const newList = list.map((value, index) => value * index); // Map to compute new array
 
 console.log(list);
-console.log(newList); 
+console.log(newList);
