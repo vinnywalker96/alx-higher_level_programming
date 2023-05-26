ce28ae11a33df25100b685e411f432f6e82e592b
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-26 15:03:02 +0000

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const file = require("fs");
+
+file.readFile(process.argv[2], 'utf-8', (error, data) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log(data);
+});
