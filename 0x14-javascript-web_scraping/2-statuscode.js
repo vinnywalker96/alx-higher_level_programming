f30e19fca5a2978b5b4e6eeb9b9bb2b24a18065e
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-26 16:47:11 +0000

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url).on('response', function(response) {
+request.get(url).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });
