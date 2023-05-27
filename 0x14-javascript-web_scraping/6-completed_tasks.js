cf82bf1d3f02835930dab7687f6610f1ed6aad43
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-27 15:12:07 +0000

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// API URL
+const apiUrl = process.argv[2];
+
+// Make a request to the API
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    const todos = JSON.parse(body);
+
+    // Compute the number of completed tasks by user id
+    const completedTasksByUserId = todos.reduce((result, todo) => {
+      if (todo.completed) {
+        const userId = todo.userId;
+        result[userId] = (result[userId] || 0) + 1;
+      }
+      return result;
+    }, {});
+
+    // Print users with completed tasks
+    Object.entries(completedTasksByUserId).forEach(([userId, count]) => {
+      console.log(` ${userId}: ${count}`);
+    });
+  }
+});
