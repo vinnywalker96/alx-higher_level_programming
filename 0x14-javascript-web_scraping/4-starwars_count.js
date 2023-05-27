af244973eed2bb0e62d18960aace26985f40542e
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-27 12:07:33 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,14 +6,14 @@ const url = process.argv[2];
 const characterId = 18;
 
 request(url, (err, response, body) => {
-  try {
+  if (err) {
+    console.error(err);
+  } else {
     const films = JSON.parse(body).results;
     const filteredFilms = films.filter(film =>
       film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
     );
     const numberOfMovies = filteredFilms.length;
     console.log(numberOfMovies);
-  } catch (error) {
-    console.error(err);
   }
 });
