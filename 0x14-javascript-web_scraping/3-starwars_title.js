e2cba20bb7609786f1396325fdeda9b048c94388
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-05-27 08:36:59 +0000

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const id = process.argv[2];
+const options = {
+  url: `https://swapi-api.alx-tools.com/api/films/${id}`,
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    'Accept-Charset': 'utf-8',
+    'User-Agent': 'my-client'
+  }
+};
+
+request(options, function(err, res, body) {
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
